08874e97f6b0cc6c6b8aaa6d8acc7d01f82bcd8d
Tim French <tfrench15@gmail.com>
2019-02-27 15:11:43 -0800

    go: isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -8,10 +8,9 @@ import (
 // IsIsogram determines whether a given string is an isogram.
 func IsIsogram(s string) bool {
 	s = strings.ToLower(s)
-	seen := make(map[string]bool)
-	for i := 0; i < len(s); i++ {
-		char := string(s[i])
-		if !unicode.IsLetter(rune(char)) {
+	seen := make(map[rune]bool)
+	for _, char := range s {
+		if !unicode.IsLetter(char) {
 			continue
 		}
 		if ok := seen[char]; ok {
