abce19c8f3186601a8bf81675fce06619dd2eeda
Tim French <tfrench15@gmail.com>
2019-02-28 11:25:11 -0800

    go: stub out grains problem

---
+++ go/grains/grains.go
@@ -0,0 +1,13 @@
+package grains
+
+// Square returns the number of grains of wheat
+// on a particular square of the chessboard.
+func Square(num int) (uint64, error) {
+	return 0, nil
+}
+
+// Total returns the total number of grains of wheat
+// on the chessboard.
+func Total() uint64 {
+	return 0
+}
