c781ca03426dfc65d0039759016e236d86c13367
Tim French <tfrench15@gmail.com>
2019-02-28 15:32:23 -0800

    go: finish grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,13 +1,34 @@
 package grains
 
+import (
+	"errors"
+	"math"
+)
+
 // Square returns the number of grains of wheat
 // on a particular square of the chessboard.
 func Square(num int) (uint64, error) {
-	return 0, nil
+	if num <= 0 {
+		return 0, errors.New("argument must be an integer >= zero")
+	}
+	if num > 64 {
+		return 0, errors.New("argument must be <= 64")
+	}
+	fnum := float64(num)
+	ans := math.Exp2(fnum - 1)
+	return uint64(ans), nil
 }
 
 // Total returns the total number of grains of wheat
 // on the chessboard.
 func Total() uint64 {
-	return 0
+	var sum uint64
+	for i := 1; i <= 64; i++ {
+		sq, err := Square(i)
+		if err != nil {
+			panic(err)
+		}
+		sum += sq
+	}
+	return sum
 }
