57c977a1fe948a02c1a50ed5dc2314d9db5070c6
Tim French <tfrench15@gmail.com>
2019-02-28 23:00:25 -0800

    go: finish clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,5 +1,7 @@
 package clock
 
+import "fmt"
+
 // Clock represents a clock, holding time.
 type Clock struct {
 	hour   int
@@ -7,21 +9,75 @@ type Clock struct {
 }
 
 // New returns a new Clock.
-func New(hour, minute int) Clock {
-	return Clock{}
+func New(hours, minutes int) Clock {
+	var minsToHrs int
+
+	if minutes < 0 {
+		for minutes < 0 {
+			minutes += 60
+			minsToHrs--
+		}
+	} else {
+		minsToHrs = minutes / 60
+	}
+	minutes = minutes % 60
+
+	hours += minsToHrs
+	for hours < 0 {
+		hours += 24
+	}
+	hours = hours % 24
+
+	return Clock{
+		hour:   hours,
+		minute: minutes,
+	}
 }
 
 // String satisfies the Stringer interface for Clock.
-func (c *Clock) String() string {
-	return ""
+func (c Clock) String() string {
+	var (
+		hour   string
+		minute string
+	)
+
+	if c.hour < 10 {
+		hour = fmt.Sprintf("0%d", c.hour)
+	} else {
+		hour = fmt.Sprintf("%d", c.hour)
+	}
+
+	if c.minute < 10 {
+		minute = fmt.Sprintf("0%d", c.minute)
+	} else {
+		minute = fmt.Sprintf("%d", c.minute)
+	}
+
+	return fmt.Sprintf("%s:%s", hour, minute)
 }
 
 // Add adds the given minutes to the current Clock.
 func (c Clock) Add(minutes int) Clock {
+	c.minute += minutes
+	newHrs := c.minute / 60
+	c.minute = c.minute % 60
+
+	c.hour += newHrs
+	c.hour = c.hour % 24
 	return c
 }
 
 // Subtract subtracts the given minutes to the current Clock.
 func (c Clock) Subtract(minutes int) Clock {
+	var hrsToSubtract int
+	c.minute -= minutes
+	for c.minute < 0 {
+		c.minute += 60
+		hrsToSubtract++
+	}
+	c.hour -= hrsToSubtract
+	for c.hour < 0 {
+		c.hour += 24
+	}
 	return c
 }
