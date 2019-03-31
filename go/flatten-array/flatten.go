44d429df16eeb10d23a28d40d19394d77b163ab7
Tim French <tfrench15@gmail.com>
2019-03-31 12:02:57 -0700

    go: flatten array

--- go/flatten-array/flatten.go
+++ go/flatten-array/flatten.go
@@ -2,6 +2,16 @@ package flatten
 
 // Flatten takes a nested slice and returns a flattened
 // version of the slice.
-func Flatten(slc []interface{}) []interface{} {
-	return nil
+func Flatten(slc interface{}) []interface{} {
+	ret := []interface{}{}
+
+	switch elem := slc.(type) {
+	case []interface{}:
+		for _, v := range elem {
+			ret = append(ret, Flatten(v)...)
+		}
+	case int:
+		ret = append(ret, elem)
+	}
+	return ret
 }
