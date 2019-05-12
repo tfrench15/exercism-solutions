bc9b81c05afd818c17fe9709fa2adfb7b2c3ff52
Tim French <tfrench15@gmail.com>
2019-05-11 19:12:29 -0500

    rust: finish etl

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,5 +1,12 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    unimplemented!("How will you transform the tree {:?}?", h)
+    let mut new_system = BTreeMap::new();
+
+    for (key, value) in h.iter() {
+        for ch in value {
+            new_system.entry(ch.to_ascii_lowercase()).or_insert(*key);
+        }
+    }
+    new_system
 }
