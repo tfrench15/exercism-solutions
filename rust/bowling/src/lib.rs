544ccffc7d59545b9b7dcba942663b68558e75fc
Tim French <tfrench15@gmail.com>
2019-07-04 15:00:27 -0400

    rust: bowling still not finished

--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -4,18 +4,111 @@ pub enum Error {
     GameComplete,
 }
 
-pub struct BowlingGame {}
+pub struct BowlingGame {
+    frames: Vec<Frame>,
+}
+
+#[derive(PartialEq)]
+enum FrameStatus {
+    Incomplete,
+    Strike,
+    Spare,
+    Open,
+}
+
+struct Frame {
+    status: FrameStatus,
+    rolls: u16,
+    pins: u16,
+    score: u16,
+}   
+
+impl Frame {
+    fn new() -> Self {
+        Frame {
+            status: FrameStatus::Incomplete,
+            rolls: 0,
+            pins: 10,
+            score: 0, 
+        }
+    }
+
+    fn roll(&mut self, pins: u16) -> Result<(), Error> {
+        match self.rolls {
+            0 => {
+                self.rolls += 1;
+                self.pins -= pins;
+                if self.pins == 0 {
+                    self.status = FrameStatus::Strike;
+                    return Ok(())
+                } else if self.pins < 0 {
+                    return Err(Error::NotEnoughPinsLeft)
+                } else {
+                    return Ok(())
+                }
+            },
+            1 => { 
+                self.rolls += 1;
+                self.pins -= pins;
+                if self.pins == 0 {
+                    self.status = FrameStatus::Spare;
+                    return Ok(())
+                }
+                if self.pins < 0 {
+                    return Err(Error::NotEnoughPinsLeft)
+                } else {
+                    self.status = FrameStatus::Open;
+                    return Ok(())
+                }
+            },
+            _ => {
+                return Ok(())
+            }
+        }
+    }
+}
 
 impl BowlingGame {
     pub fn new() -> Self {
-        unimplemented!();
+        BowlingGame { frames: Vec::new() }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        if self.frames.len() == 10 && self.frames[9].status != FrameStatus::Incomplete {
+            return Err(Error::GameComplete)
+        };
+
+        if self.frames.len() == 0 {
+            self.frames.push(Frame::new());
+        };
+
+        match self.frames.last().unwrap().status {
+            FrameStatus::Incomplete => { self.frames.last_mut().unwrap().roll(pins) },
+            FrameStatus::Strike => { 
+                self.frames.push(Frame::new());
+                self.frames.last_mut()
+                           .unwrap()
+                           .roll(pins)
+            },
+            FrameStatus::Spare => { 
+                self.frames.push(Frame::new());
+                self.frames.last_mut()
+                           .unwrap()
+                           .roll(pins)
+            },
+            FrameStatus::Open => { 
+                self.frames.push(Frame::new());
+                self.frames.last_mut() 
+                           .unwrap()
+                           .roll(pins)
+            },
+        }
     }
 
     pub fn score(&self) -> Option<u16> {
-        unimplemented!("Return the score if the game is complete, or None if not.");
+        match self.frames.len() {
+            10 => { Some(90) }, // TODO: tabulate score
+            _ => { None }
+        }
     }
 }
