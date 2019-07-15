9e80d3a6c1fcf864bf85a033c30d0d70f87e87f3
Tim French <tfrench15@gmail.com>
2019-07-15 09:12:55 -0700

    rust: tournament, start building

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -1,6 +1,60 @@
+use std::collections::HashMap;
+
+struct Record {
+    matches_played: u32,
+    wins: u32,
+    draws: u32,
+    losses: u32,
+    points: u32,
+}
+
 pub fn tally(match_results: &str) -> String {
-    unimplemented!(
-        "Given the result of the played matches '{}' return a properly formatted tally table string.",
-        match_results
-    );
+    let header = String::from("Team                           | MP |  W |  D |  L |  P");
+    if match_results.len() == 0 {
+        return header
+    }
+
+    let mut records = HashMap::new();
+
+    for game in match_results.lines() {
+        let result: Vec<&str> = game.split(";").collect();
+        match result[2] {
+            "win" => { 
+                let winning_team = records.entry(result[0]).or_insert(Record::new());
+                *winning_team.matches_played += 1;
+                *winning_team.wins += 1;
+                *winning_team.points += 3;
+                let losing_team = records.entry(result[1]).or_insert(Record::new());
+                *losing_team.matches_played += 1;
+                *losing_team.losses += 1;
+            },
+            "loss" => { 
+                let winning_team = records.entry(result[1]).or_insert(Record::new());
+                *winning_team.matches_played += 1;
+                *winning_team.wins += 1;
+                *winning_team.points += 3;
+                let losing_team = records.entry(result[0]).or_insert(Record::new());
+                *losing_team.matches_played += 1;
+                *losing_team.losses += 1;
+            },
+            "draw" => { 
+                let team_one = records.entry(result[0]).or_insert(Record::new());
+                *team_one.matches_played += 1;
+                *team_one.draws += 1;
+                *team_one.points += 1;
+                let team_two = records.entry(result[1]).or_insert(Record::new());
+                *team_two.matches_played += 1;
+                *team_two.draws += 1;
+                *team_two.points += 1;
+            },
+        }
+    }
+
+    String::new()
+}
+
+impl Record {
+    fn new() -> Self {
+        Record { 0, 0, 0, 0, 0}
+    }
 }
