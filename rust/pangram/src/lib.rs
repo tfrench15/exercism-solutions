a12f1d41c29555150a19b520be0853c0680b4474
Tim French <tfrench15@gmail.com>
2019-07-28 16:31:40 -0700

    rust: pangram finish

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,4 +1,17 @@
+use std::collections::HashMap;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    let mut alphabet = HashMap::new();
+
+    for ch in sentence.to_lowercase().chars() {
+        if !ch.is_ascii_alphabetic() {
+            continue
+        } else {
+            let counter = alphabet.entry(ch).or_insert(0);
+            *counter += 1;
+        }
+    }
+
+    alphabet.len() == 26
 }
