158205fe035fed2157fbdf31d0b3261e0d44261a
Tim French <tfrench15@gmail.com>
2019-07-28 11:18:05 -0700

    rust: saddle-points 9/10

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,11 +1,13 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let mut saddle_points: Vec<(usize, usize)> = Vec::new();
 
-    for row in input {
-        let maxes = find_max_in_row(row);
-        for pair in &maxes {
-            if is_min_in_column(pair.0 as usize, pair.1, input) {
-                saddle_points.push(*pair);
+    for row in 0..input.len() {
+        let max = find_max_in_row(&input[row]);
+        for (idx, val) in input[row].iter().enumerate() {
+            if *val == max {
+                if is_min_in_column(idx, *val, input) {
+                    saddle_points.push((row, idx));
+                }
             }
         }
     }
@@ -13,9 +15,9 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     saddle_points
 }
 
-fn is_min_in_column(idx: usize, num: usize, matrix: &[Vec<u64>]) -> bool {
+fn is_min_in_column(idx: usize, num: u64, matrix: &[Vec<u64>]) -> bool {
     for row in matrix {
-        if row[idx] < num as u64 {
+        if row[idx] < num {
             return false
         }
     }
@@ -23,25 +25,14 @@ fn is_min_in_column(idx: usize, num: usize, matrix: &[Vec<u64>]) -> bool {
     true
 }
 
-fn find_max_in_row(row: &[u64]) -> Vec<(usize, usize)> {
-    let mut maxes: Vec<(usize, usize)> = Vec::new();
+fn find_max_in_row(row: &[u64]) -> u64 {
     let mut max = row[0];
-    let mut idx = 0;
 
-    maxes.push((idx, max as usize));
-
-    for (i, val) in row.iter().enumerate().skip(1) {
-        if val > &max {
-            maxes.clear();
-            max = *val;
-            idx = i;
-            maxes.push((idx, max as usize));
-        } else if *val == max {
-            maxes.push((i, *val as usize));
-        } else {
-            continue
-        }
+    for num in row.iter().skip(1) {
+        if num > &max {
+            max = *num;
+        } 
     }
 
-    maxes
+    max
 }
