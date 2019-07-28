ca59844918dbc1330a651a3d30b034f2021cb666
Tim French <tfrench15@gmail.com>
2019-07-28 11:04:52 -0700

    rust: saddle-points 1/10

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,11 +1,21 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let mut saddle_points: Vec<(usize, usize)> = Vec::new();
 
+    for row in input {
+        let maxes = find_max_in_row(row);
+        for pair in &maxes {
+            if is_min_in_column(pair.0 as usize, pair.1, input) {
+                saddle_points.push(*pair);
+            }
+        }
+    }
+
+    saddle_points
 }
 
-fn is_min_in_column(num: u64, idx: usize, matrix: &[Vec<u64>]) -> bool {
+fn is_min_in_column(idx: usize, num: usize, matrix: &[Vec<u64>]) -> bool {
     for row in matrix {
-        if row[idx] < num {
+        if row[idx] < num as u64 {
             return false
         }
     }
@@ -13,20 +23,25 @@ fn is_min_in_column(num: u64, idx: usize, matrix: &[Vec<u64>]) -> bool {
     true
 }
 
-fn max_in_row(matrix: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let possible_saddle_points: Vec<(usize, usize)> = Vec::new();
+fn find_max_in_row(row: &[u64]) -> Vec<(usize, usize)> {
+    let mut maxes: Vec<(usize, usize)> = Vec::new();
+    let mut max = row[0];
+    let mut idx = 0;
 
-    for row in matrix {
-        let mut idx = 0;
-        let mut max = row[0];
+    maxes.push((idx, max as usize));
 
-        for i, val in row.enumerate().skip(1) {
-            if val > max {
-                idx = i;
-                max = val;
-            }
-
-            
+    for (i, val) in row.iter().enumerate().skip(1) {
+        if val > &max {
+            maxes.clear();
+            max = *val;
+            idx = i;
+            maxes.push((idx, max as usize));
+        } else if *val == max {
+            maxes.push((i, *val as usize));
+        } else {
+            continue
         }
     }
+
+    maxes
 }
