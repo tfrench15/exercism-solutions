3ba5da2bb35dfd62ab0b8a6f0e91573310f96d88
Tim French <tfrench15@gmail.com>
2019-07-28 13:17:00 -0700

    rust: isogram finish, some go stuff too

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -3,14 +3,9 @@ package erratum
 // Use is the function required for this exercise.
 func Use(o ResourceOpener, input string) error {
 	resource, err := o()
-	defer resource.Close()
-	switch err {
-	case nil:
-		resource.Frob(input)
-		return nil
-	case TransientError{err}:
-		return Use(o, input)
-	default:
-		return err
+	if err != nil {
+		if e, ok := err.(TransientError) {
+			return o()
+		}
 	}
 }
--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,3 +1,24 @@
+use std::collections::HashMap;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    if candidate.len() == 0 {
+        return true
+    }
+
+    let mut counts = HashMap::new();
+    for ch in candidate.to_lowercase().chars() {
+        if !ch.is_alphabetic() {
+            continue
+        }
+        let counter = counts.entry(ch).or_insert(0);
+        *counter += 1;
+    }
+
+    for value in counts.values() {
+        if *value > 1 {
+            return false
+        }
+    }
+
+    true
 }
