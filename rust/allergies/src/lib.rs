e86f798d4a3ba445ea0b4dcd97bb70f2c624ec40
Tim French <tfrench15@gmail.com>
2019-07-30 09:25:00 -0700

    rust: allergies add helpers and pow 2 cases

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -18,19 +18,53 @@ pub enum Allergen {
 impl Allergies {
     pub fn new(score: u32) -> Self {
         let mut allergies = Vec::new();
-        if score.is_power_of_two() {
-            allergies.push(score.next_power_of_two());
-        } else {
-            let upper_bound = score.next_power_of_two();
-            
+        match score.is_power_of_two() {
+            true => { 
+                match score.next_power_of_two() {
+                    1 => { allergies.push(Allergen::Eggs); },
+                    2 => { allergies.push(Allergen::Peanuts); },
+                    4 => { allergies.push(Allergen::Shellfish); },
+                    8 => { allergies.push(Allergen::Strawberries); },
+                    16 => { allergies.push(Allergen::Tomatoes); },
+                    32 => { allergies.push(Allergen::Chocolate); },
+                    64 => { allergies.push(Allergen::Pollen); },
+                    128 => { allergies.push(Allergen::Cats); },
+                    _ => { continue },
+                };
+            },
+            false => { 
+                match score {
+                    0 => { continue },
+                    3 => { 
+                        allergies.push(Allergen::Eggs);
+                        allergies.push(Allergen::Peanuts);
+                    },
+                    5..=7 {
+                        allergies.push(Allergen::Shellfish);
+                        score -= 
+                    }
+                }
+            }
         }
+
+        Allergies { foods: allergies }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        false
+        self.foods.contains(allergen)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
         self.foods.clone()
     }
+}
+
+fn compute_allergies(score: u32, allergies: Vec<Allergen>) -> Vec<Allergen> {
+    let prev_pow = previous_power_of_two(score);
+
+    match prev_pow 
+}
+
+fn previous_power_of_two(score: u32) -> u32 {
+    score.next_power_of_two() / 2
 }
