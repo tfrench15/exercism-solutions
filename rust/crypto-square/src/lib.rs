36381b0fb4dd929ab0f7fb321b20795c6d9dd011
Tim French <tfrench15@gmail.com>
2019-08-01 18:45:30 -0700

    rust: crypto-square compute columns

--- rust/crypto-square/src/lib.rs
+++ rust/crypto-square/src/lib.rs
@@ -1,9 +1,22 @@
 pub fn encrypt(input: &str) -> String {
+    if input.len() == 0 {
+        return "".to_string()
+    }
+
     let normalized: String = input
         .chars()
         .filter(|ch| ch.is_ascii_alphabetic())
         .map(|ch| ch.to_ascii_lowercase())
         .collect();
-    
-    normalized
+
+    let cols = columns(normalized.len());
+}
+
+fn columns(length: usize) -> usize {
+    let mut i: usize = 1;
+    while i.pow(2) < length {
+        i += 1;
+    }
+
+    i
 }
