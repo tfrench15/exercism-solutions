6ecc63aa7b5acf3c42aca25c6241652eed9f0a77
Tim French <tfrench15@gmail.com>
2019-08-01 20:05:53 -0700

    rust: crypto-square 5/6

--- rust/crypto-square/src/lib.rs
+++ rust/crypto-square/src/lib.rs
@@ -10,7 +10,26 @@ pub fn encrypt(input: &str) -> String {
         .collect();
 
     let cols = columns(normalized.len());
+
+    let chars: Vec<char> = normalized
+        .chars()
+        .collect();
+    
+    let mut vec_of_words: Vec<String> = Vec::new();
+
+    for chunk in chars.chunks(cols) {
+        let word: String = chunk
+            .iter()
+            .collect();
+        
+        vec_of_words.push(word);
+    }
+
+    let encrypted_words = apply_cipher(&vec_of_words);
+
+    encrypted_words.join(" ")
 }
+
 
 fn columns(length: usize) -> usize {
     let mut i: usize = 1;
@@ -19,4 +38,27 @@ fn columns(length: usize) -> usize {
     }
 
     i
+}
+
+fn apply_cipher(words: &[String]) -> Vec<String> {
+    let mut idx: usize = 0;
+    let mut encrypted_words = Vec::new();
+
+    while idx < words[0].len() {
+        let mut encrypted_word = String::new();
+        for j in 0..words.len() {
+            match words[j].get(idx..idx+1) {
+                None => { 
+                    encrypted_word.push_str(" "); 
+                },
+                Some(letter) => { 
+                    encrypted_word.push_str(letter);
+                }
+            }
+        }
+        encrypted_words.push(encrypted_word);
+        idx += 1;
+    }
+
+    encrypted_words
 }
