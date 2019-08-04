45de3dbd9b1a2b499c2aedadb5313b47554e3f84
Tim French <tfrench15@gmail.com>
2019-08-04 12:25:32 -0700

    rust: anagram 11/12

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,28 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut anagrams = HashSet::new();
+    let mut word_map = HashMap::new();
+
+    for ch in word.chars().map(|ch| ch.to_ascii_lowercase()) {
+        let count = word_map.entry(ch).or_insert(0);
+        *count += 1;
+    }
+
+    for candidate in possible_anagrams.iter() {
+        if candidate.to_lowercase() == word.to_lowercase() {
+            continue
+        } else {
+            let mut candidate_map = HashMap::new();
+            for ch in candidate.chars().map(|ch| ch.to_ascii_lowercase()) {
+                let count = candidate_map.entry(ch).or_insert(0);
+                *count += 1;
+            }
+            if word_map == candidate_map {
+                anagrams.insert(*candidate);
+            }
+        }   
+    }
+
+    anagrams
 }
