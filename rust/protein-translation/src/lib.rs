78bb55e0f0d691823e37686fa43ea64d048d54f1
Tim French <tfrench15@gmail.com>
2019-08-05 23:32:35 -0700

    rust: protein-translation of_rna debugging

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -1,43 +1,36 @@
 use std::collections::HashMap;
 
 pub struct CodonsInfo<'a> {
-    map: HashMap<&'static str, &'static str>
+    map: HashMap<&'a str, &'a str>
 }
 
 impl<'a> CodonsInfo<'a> {
-    pub fn new() -> Self {
-        let pairs = make_pairs();
-        let mut mappings = HashMap::new();
-
-        for (codon, protein) in pairs {
-            mappings.insert(codon, protein);
-        }
-
-        CodonsInfo {
-            map: mappings
+    pub fn name_for(&self, codon: &'a str) -> Option<&'a str> {
+        match self.map.get(codon) {
+            None => { None },
+            Some(v) => { Some(v) },
         }
     }
-    
-    pub fn name_for(&self, codon: &str) -> Option<&'a str> {
-        self.map.get(codon)
-    }
 
-    pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
+    pub fn of_rna(&self, rna: &'a str) -> Option<Vec<&'a str>> {
         let mut proteins = Vec::new();
         if rna.len() % 3 != 0 {
             return None
         } else {
-            for chunk in rna.chars().chunks(3) {
-                match self.map.get(chunk) {
+            let chars: Vec<char> = rna
+                .chars()
+                .collect();
+            for chunk in chars.chunks(3) {
+                match self.map.get(&chunk) {
                     None => { return None },
                     Some(v) => { 
-                        proteins.push(v);
+                        proteins.push(*v);
                     },
                 }
             }
         }
 
-        proteins
+        Some(proteins)
     }
 }
 
