bc086a8acad0e5013d9b4bbd48b23be8a954a21a
Tim French <tfrench15@gmail.com>
2019-08-06 08:34:44 -0700

    rust: robot-name stub out

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,9 +1,18 @@
+use rand::{thread_rng, Rng};
+
 pub struct Robot {
     name: String
 }
 
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 impl Robot {
     pub fn new() -> Self {
+        let mut name = String::new();
+        for i in 0..5 {
+            
+        }
+
         Robot {
             name: String::new()
         }
