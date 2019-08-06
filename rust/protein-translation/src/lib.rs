343e64c6a787e1bcb908cf86c0666d8b3f38a025
Tim French <tfrench15@gmail.com>
2019-08-06 00:27:38 -0700

    rust: protein-translation finish

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::borrow::Borrow;
 
 pub struct CodonsInfo<'a> {
     map: HashMap<&'a str, &'a str>
@@ -13,7 +14,32 @@ impl<'a> CodonsInfo<'a> {
     }
 
     pub fn of_rna(&self, rna: &'a str) -> Option<Vec<&'a str>> {
-        unimplemented!()
+        if rna.len() == 0 || rna.len() % 3 != 0 {
+            return None
+        }
+
+        let mut proteins = Vec::new();
+
+        let owned_strand: Vec<char> = rna
+            .to_owned()
+            .chars()
+            .collect();
+
+        for chunk in owned_strand.chunks(3) {
+            let encoding: String = chunk.iter().collect();
+            match self.map.get(&encoding.borrow()) {
+                None => { return None },
+                Some(v) => { 
+                    if *v == "stop codon" {
+                        break
+                    } else {
+                        proteins.push(*v); 
+                    }
+                }
+            }
+        }
+
+        Some(proteins)
     }
 }
 
