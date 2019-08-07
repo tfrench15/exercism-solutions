a1a51b3ce7a70c0ccd7b5f7f9f4ee765dea8484b
Tim French <tfrench15@gmail.com>
2019-08-06 19:13:22 -0700

    rust: rotational-cipher download and start

---
+++ rust/rotational-cipher/src/lib.rs
@@ -0,0 +1,11 @@
+
+const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWERCASE: &str = "abcdefghijklmmopqrstuvwxyz";
+
+pub fn rotate(input: &str, key: i8) -> String {
+    input
+        .chars()
+        .for_each(|ch| {
+            
+        })
+}
