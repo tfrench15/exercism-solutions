5958deac8d21a6e9ab54d7e6766221175a40b1ca
Tim French <tfrench15@gmail.com>
2019-08-06 18:47:30 -0700

    rust: robot-name finish

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,21 +1,30 @@
-use rand::{thread_rng, Rng};
+use rand::{Rng};
 
 pub struct Robot {
     name: String
 }
 
-const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 impl Robot {
     pub fn new() -> Self {
         let mut name = String::new();
-        for i in 0..5 {
-            
+        let mut idx = name.len();
+        
+        while idx < 5 {
+            if idx == 0 || idx == 1 {
+                let letter = generate_random_letter();
+                name.push(letter);
+                idx += 1;
+            } else {
+                let number = generate_random_number();
+                let s = number.to_string();
+                name.push_str(&s);
+                idx += 1;
+            }
         }
 
-        Robot {
-            name: String::new()
-        }
+        Robot { name: name }
     }
 
     pub fn name(&self) -> &str {
@@ -23,6 +32,22 @@ impl Robot {
     }
 
     pub fn reset_name(&mut self) {
-        self = self.new();
+        let new_robot = Robot::new();
+        self.name = new_robot.name.to_string();
     }
+}
+
+fn generate_random_letter() -> char {
+    let mut rng = rand::thread_rng();
+    let rand_num: u32 = rng.gen_range(0, 26);
+
+    ALPHABET
+        .chars()
+        .nth(rand_num as usize)
+        .unwrap()
+}
+
+fn generate_random_number() -> u32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0, 10)
 }
