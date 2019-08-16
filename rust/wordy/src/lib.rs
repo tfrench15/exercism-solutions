521867c80b95837714c716edcc94303ecc4a19ee
Tim French <tfrench15@gmail.com>
2019-08-15 21:36:43 -0400

    rust: wordy tinker around

--- rust/wordy/src/lib.rs
+++ rust/wordy/src/lib.rs
@@ -1,14 +1,34 @@
-pub struct WordProblem;
+pub struct WordProblem {
+    tup: (Option<i32>, Option<Operation>, Option<i32>);
+}
+
+enum Operation {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+}
+
+impl WordProblem {
+    fn new() -> Self {
+        WordProblem {
+            tup: (None, None, None),
+        }
+    }
+}
 
 pub fn answer(command: &str) -> Option<i32> {
     if !command.starts_with("What is") {
         return None
     }
 
-    let _command_vec: Vec<&str> = command
+    let problem: Vec<&str> = command
         .split_ascii_whitespace()
         .skip(2)
         .collect();
 
     Some(5)
 }
+
+
+
